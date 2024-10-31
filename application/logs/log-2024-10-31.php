f3bf8d655cbb37884fe735cd41d0ba5c2940ae11
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2024-10-31 15:56:44 +0700

    lanjut point 11

---
+++ application/logs/log-2024-10-31.php
@@ -0,0 +1,112 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-31 05:10:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'eduappgt_app'@'localhost' (using password: YES) C:\xampp\htdocs\nazarethnet\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2024-10-31 05:10:01 --> Unable to connect to the database
+ERROR - 2024-10-31 05:10:01 --> Severity: error --> Exception: Class 'EduAppGT' not found C:\xampp\htdocs\nazarethnet\system\core\CodeIgniter.php 370
+ERROR - 2024-10-31 05:10:14 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'eduappgt_app'@'localhost' (using password: YES) C:\xampp\htdocs\nazarethnet\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2024-10-31 05:10:14 --> Unable to connect to the database
+ERROR - 2024-10-31 05:10:14 --> Severity: error --> Exception: Class 'EduAppGT' not found C:\xampp\htdocs\nazarethnet\system\core\CodeIgniter.php 370
+ERROR - 2024-10-31 02:04:41 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:04:41 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:04:41 --> Severity: Warning --> include(/attendance_report.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:04:41 --> Severity: Warning --> include(): Failed opening '/attendance_report.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:26:40 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:26:40 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:26:40 --> Severity: Warning --> include(/attendance_report.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:26:40 --> Severity: Warning --> include(): Failed opening '/attendance_report.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:26:43 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:26:43 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:26:43 --> Severity: Warning --> include(/attendance_report.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:26:43 --> Severity: Warning --> include(): Failed opening '/attendance_report.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:27:04 --> Severity: Warning --> include(/navigation.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:27:04 --> Severity: Warning --> include(): Failed opening '/navigation.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 41
+ERROR - 2024-10-31 02:27:04 --> Severity: Warning --> include(/attendance_report.php): failed to open stream: No such file or directory C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:27:04 --> Severity: Warning --> include(): Failed opening '/attendance_report.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\nazarethnet\application\views\backend\index.php 42
+ERROR - 2024-10-31 02:27:12 --> Severity: Notice --> session_start(): A session had already been started - ignoring C:\xampp\htdocs\nazarethnet\public\face\config.php 2
+ERROR - 2024-10-31 02:27:18 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:27:18 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:27:18 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 02:27:30 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:27:30 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:27:30 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 02:29:22 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:29:22 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:29:22 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 02:36:54 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:36:54 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:36:55 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 02:39:05 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:39:05 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:39:05 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 02:41:19 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 02:41:19 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 02:41:19 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:05:13 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:05:13 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:05:13 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:06:09 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:06:09 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:06:09 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:07:40 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:07:40 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:07:40 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:08:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:08:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:08:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:08:44 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:08:44 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:08:44 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:09:00 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:09:00 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:09:00 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:10:12 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:10:12 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:10:12 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:10:45 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:10:45 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:10:45 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:13:49 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:13:49 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:13:49 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:14:21 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:14:21 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:14:21 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:41:32 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:41:33 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:41:33 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:43:14 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:43:14 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:43:14 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:43:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:43:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:43:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:43:42 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
+ERROR - 2024-10-31 03:43:42 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'first_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2878
+ERROR - 2024-10-31 03:43:43 --> Severity: Notice --> Trying to get property 'last_name' of non-object C:\xampp\htdocs\nazarethnet\application\models\Crud.php 2879
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:05 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:17 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:18 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:22 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:27 --> 404 Page Not Found: Public/uploads
+ERROR - 2024-10-31 03:44:33 --> 404 Page Not Found: Public/uploads
