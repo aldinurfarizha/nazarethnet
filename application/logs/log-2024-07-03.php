e30168be3ab4a93bba44009d2b9a2dceab31ef9f
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2025-05-19 09:30:32 +0700

    Ignore .htaccess and application/logs

--- application/logs/log-2024-07-03.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-07-03 06:17:12 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
- C:\xampp\htdocs\nazarethnet\system\database\drivers\mysqli\mysqli_driver.php 211
-ERROR - 2024-07-03 06:17:12 --> Unable to connect to the database
-ERROR - 2024-07-03 06:17:12 --> Severity: error --> Exception: Class 'EduAppGT' not found C:\xampp\htdocs\nazarethnet\system\core\CodeIgniter.php 370
--- application/logs/log-2024-10-30.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2024-10-30 23:13:55 --> Severity: Notice --> session_start(): A session had already been started - ignoring C:\xampp\htdocs\nazarethnet\public\face\config.php 2
-ERROR - 2024-10-30 23:14:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
-ERROR - 2024-10-30 23:14:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
-ERROR - 2024-10-30 23:14:04 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
-ERROR - 2024-10-30 23:34:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 71
-ERROR - 2024-10-30 23:34:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 298
-ERROR - 2024-10-30 23:34:38 --> Severity: Notice --> Trying to get property 'permissions' of non-object C:\xampp\htdocs\nazarethnet\application\views\backend\admin\navigation.php 549
