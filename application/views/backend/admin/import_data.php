c1a3153716ffcff6123d9a96b691cdce0e71ac9e
Aldi Nurfarizha <55512360+aldinurfarizha@users.noreply.github.com>
2025-05-19 12:16:05 +0700

    exam import hold waiting answer

--- application/views/backend/admin/import_data.php
+++ application/views/backend/admin/import_data.php
@@ -85,65 +85,65 @@
     																	<i class="fas fa-download"></i> Download Sample CSV
     																</a>
     															</div>
-																<div class="row">
-																	<div class="col-sm-3">
-																		<div class="form-group label-floating is-select">
-																			<label class="control-label"><?php echo getEduAppGTLang('branch'); ?></label>
-																			<div class="select">
-																				<select name="branch_id" required="" onchange="get_shifts(this.value); get_class(this.value);">
-																					<option value=""><?php echo getEduAppGTLang('select'); ?></option>
-																					<?php
-																					if(isSuperAdmin()){
-																						$branch = $this->db->where(['status'=>'ACTIVE'])->get('branch')->result_array();
-																					}else{
-																						$branch = $this->db->where(['branch_id'=>getMyBranchId()->branch_id,'status'=>'ACTIVE'])->get('branch')->result_array();
+    															<div class="row">
+    																<div class="col-sm-3">
+    																	<div class="form-group label-floating is-select">
+    																		<label class="control-label"><?php echo getEduAppGTLang('branch'); ?></label>
+    																		<div class="select">
+    																			<select name="branch_id" required="" onchange="get_shifts(this.value); get_class(this.value);">
+    																				<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																				<?php
+																					if (isSuperAdmin()) {
+																						$branch = $this->db->where(['status' => 'ACTIVE'])->get('branch')->result_array();
+																					} else {
+																						$branch = $this->db->where(['branch_id' => getMyBranchId()->branch_id, 'status' => 'ACTIVE'])->get('branch')->result_array();
 																					}
-																					
+
 																					foreach ($branch as $row):
 																					?>
-																						<option value="<?php echo $row['branch_id']; ?>"><?php echo $row['name']; ?></option>
-																					<?php endforeach; ?>
-																				</select>
-																			</div>
-																		</div>
-																	</div>
-																	<div class="col-sm-3">
-																		<div class="form-group label-floating is-select">
-																			<label class="control-label"><?php echo getEduAppGTLang('shifts'); ?></label>
-																			<div class="select">
-																				<select name="shifts_id" required id="shifts_holder">
-																					<option value=""><?php echo getEduAppGTLang('select'); ?></option>
-																				</select>
-																			</div>
-																		</div>
-																	</div>
-																	<div class="col-sm-3">
-																		<div class="form-group label-floating is-select">
-																			<label class="control-label"><?php echo getEduAppGTLang('class'); ?></label>
-																			<div class="select">
-																				<select name="class_id" id="class_holder" required="" onchange="get_sections(this.value)">
-																					<option value=""><?php echo getEduAppGTLang('select'); ?></option>
-																				</select>
-																			</div>
-																		</div>
-																	</div>
-																	<div class="col-sm-3">
-																		<div class="form-group label-floating is-select">
-																			<label class="control-label"><?php echo getEduAppGTLang('section'); ?></label>
-																			<div class="select">
-																				<select name="section_id" required id="section_holder">
-																					<option value=""><?php echo getEduAppGTLang('select'); ?></option>
-																				</select>
-																			</div>
-																		</div>
-																	</div>
-																	<div class="col-sm-3">
-																		<div class="form-group">
-																			<label class="control-label"><?php echo getEduAppGTLang('roll'); ?></label>
-																				<input type="text" name="roll" class="form-control">
-																		</div>
-																	</div>
-																</div>
+    																					<option value="<?php echo $row['branch_id']; ?>"><?php echo $row['name']; ?></option>
+    																				<?php endforeach; ?>
+    																			</select>
+    																		</div>
+    																	</div>
+    																</div>
+    																<div class="col-sm-3">
+    																	<div class="form-group label-floating is-select">
+    																		<label class="control-label"><?php echo getEduAppGTLang('shifts'); ?></label>
+    																		<div class="select">
+    																			<select name="shifts_id" required id="shifts_holder">
+    																				<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																			</select>
+    																		</div>
+    																	</div>
+    																</div>
+    																<div class="col-sm-3">
+    																	<div class="form-group label-floating is-select">
+    																		<label class="control-label"><?php echo getEduAppGTLang('class'); ?></label>
+    																		<div class="select">
+    																			<select name="class_id" id="class_holder" required="" onchange="get_sections(this.value)">
+    																				<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																			</select>
+    																		</div>
+    																	</div>
+    																</div>
+    																<div class="col-sm-3">
+    																	<div class="form-group label-floating is-select">
+    																		<label class="control-label"><?php echo getEduAppGTLang('section'); ?></label>
+    																		<div class="select">
+    																			<select name="section_id" required id="section_holder">
+    																				<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																			</select>
+    																		</div>
+    																	</div>
+    																</div>
+    																<div class="col-sm-3">
+    																	<div class="form-group">
+    																		<label class="control-label"><?php echo getEduAppGTLang('roll'); ?></label>
+    																		<input type="text" name="roll" class="form-control">
+    																	</div>
+    																</div>
+    															</div>
     															<div class="form-group mt-4 text-end">
     																<button type="submit" class="btn btn-success">
     																	<i class="picons-thin-icon-thin-0089_upload_file"></i> Import Data
@@ -158,8 +158,93 @@
 
     									<!-- GRADE TAB -->
     									<div class="tab-pane" id="grade">
-    										<div class="row">
-    											<h2>Grade</h2>
+    										<div class="row my-4">
+    											<div class="col-md-12">
+    												<div class="card shadow-sm border-0">
+    													<div class="card-body">
+    														<div class="row">
+    															<div class="col-md-6 border">
+    																<h5 class="card-title mb-3">
+    																	<i class="fas fa-file-excel text-success"></i> Import Exam Data
+    																</h5>
+    																<form action="<?= base_url('admin/import/grade'); ?>" method="POST" enctype="multipart/form-data">
+    																	<img class="img-fluid img-rounded img-responsive" src="<?= base_url('public/uploads/exam_banner.png'); ?>" alt="">
+    																	<div class="form-group">
+    																		<label for="upload_exam"><strong>Select Excel/CSV File:</strong></label>
+    																		<input type="file" name="upload_student" id="upload_student" accept=".xlsx" class="form-control" required>
+    																	</div>
+    																	<div class="form-group mt-3">
+    																		<a href="<?= base_url('public/uploads/sample_exam.xlsx'); ?>" class="btn btn-sm btn-info">
+    																			<i class="fas fa-download"></i> Download Sample CSV
+    																		</a>
+    																	</div>
+    																	<div class="row">
+    																		<div class="col-sm-3">
+    																			<div class="form-group label-floating is-select">
+    																				<label class="control-label"><?php echo getEduAppGTLang('branch'); ?></label>
+    																				<div class="select">
+    																					<select name="branch_id" required="" onchange="get_shifts(this.value); get_class(this.value);">
+    																						<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																						<?php
+																							if (isSuperAdmin()) {
+																								$branch = $this->db->where(['status' => 'ACTIVE'])->get('branch')->result_array();
+																							} else {
+																								$branch = $this->db->where(['branch_id' => getMyBranchId()->branch_id, 'status' => 'ACTIVE'])->get('branch')->result_array();
+																							}
+
+																							foreach ($branch as $row):
+																							?>
+    																							<option value="<?php echo $row['branch_id']; ?>"><?php echo $row['name']; ?></option>
+    																						<?php endforeach; ?>
+    																					</select>
+    																				</div>
+    																			</div>
+    																		</div>
+    																		<div class="col-sm-3">
+    																			<div class="form-group label-floating is-select">
+    																				<label class="control-label"><?php echo getEduAppGTLang('shifts'); ?></label>
+    																				<div class="select">
+    																					<select name="shifts_id" required id="shifts_holder">
+    																						<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																					</select>
+    																				</div>
+    																			</div>
+    																		</div>
+    																		<div class="col-sm-3">
+    																			<div class="form-group label-floating is-select">
+    																				<label class="control-label"><?php echo getEduAppGTLang('class'); ?></label>
+    																				<div class="select">
+    																					<select name="class_id" id="class_holder" required="" onchange="get_sections(this.value)">
+    																						<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																					</select>
+    																				</div>
+    																			</div>
+    																		</div>
+    																		<div class="col-sm-3">
+    																			<div class="form-group label-floating is-select">
+    																				<label class="control-label"><?php echo getEduAppGTLang('section'); ?></label>
+    																				<div class="select">
+    																					<select name="section_id" required id="section_holder">
+    																						<option value=""><?php echo getEduAppGTLang('select'); ?></option>
+    																					</select>
+    																				</div>
+    																			</div>
+    																		</div>
+    																	</div>
+    																	<div class="form-group mt-4 text-end">
+    																		<button type="submit" class="btn btn-success">
+    																			<i class="picons-thin-icon-thin-0089_upload_file"></i> Import Data
+    																		</button>
+    																	</div>
+    																</form>
+    															</div>
+    															<div class="col-md-6">
+
+    															</div>
+    														</div>
+    													</div>
+    												</div>
+    											</div>
     										</div>
     									</div>
 
